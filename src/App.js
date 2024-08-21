99dc026064944990cf7dea938789cb8ed68d494f
RawdonNoronha <rawdonnoronha11@gmail.com>
2024-08-21 21:25:00 +0530

    added header and styled

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.components';
 import ShopPage from './pages/shop/shop.component';
 import { Route, Routes } from 'react-router-dom';
+import Header from './components/header/header.component';
 import ErrorPage from './pages/errorpage/errorpage.component';
 
 function App() {
   return (
     <div>
+      <Header />
       <Routes>
         <Route path='/' Component={HomePage} errorElement={<ErrorPage/ >}/>
         <Route path='shop' Component={ShopPage} errorElement={<ErrorPage/ >} /> 
