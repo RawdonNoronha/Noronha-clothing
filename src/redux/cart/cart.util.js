127c6d754efdda77672b346c2ccc4a749c6a597d
RawdonNoronha <rawdonnoronha11@gmail.com>
2024-09-05 22:06:57 +0530

    comments

--- src/redux/cart/cart.util.js
+++ src/redux/cart/cart.util.js
@@ -14,15 +14,18 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
 }
 
 export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+    //checking if item exists
     const existingCartItem = cartItems.find(
         cartItem => cartItem.id === cartItemToRemove.id
     )
 
+    //remove the entire item when only 1 item quantity is left
     if(existingCartItem.quantity === 1)
     {
         return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id )
     }
 
+    //removes only 1 from quantity
     return cartItems.map(
         cartItem => cartItem.id === cartItemToRemove.id ? {...cartItem, quantity: cartItem.quantity - 1} : cartItem
     )
