94039d933c2b42890a98d8a5634108b7b8c09d3a
RawdonNoronha <rawdonnoronha11@gmail.com>
2024-09-08 10:33:34 +0530

    before doing nested Routes

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,6 +1,10 @@
 import { combineReducers } from "redux";
+
 import userReducer from "./user/user.reducer";
 import CartReducer from "./cart/cart.reducer";
+import directoryReducer from "./directory/directory.reducer";
+import shopReducer from "./shop/shop.reducer";
+
 import persistReducer from "redux-persist/lib/persistReducer";
 import storage from "redux-persist/lib/storage";
 
@@ -11,7 +15,9 @@ const persistConfig = {
    
 const rootReducer = combineReducers({
     user: userReducer,
-    cart: CartReducer
+    cart: CartReducer,
+    directory: directoryReducer,
+    shop: shopReducer
 });
 
 export default persistReducer(persistConfig, rootReducer)
