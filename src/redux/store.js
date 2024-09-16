3313827e8baf4b6c0c575d11fa67d9b47077d26b
RawdonNoronha <rawdonnoronha11@gmail.com>
2024-09-16 23:48:51 +0530

    process development

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,5 +5,11 @@ import { persistStore } from "redux-persist";
 
 const middlewares = [logger];
 
+//process.env is an eviroment variable it will know if the app is working in production or not
+if(process.env.NODE_ENV === 'development')
+{
+    middlewares.push(logger);
+}
+
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 export const persistor = persistStore(store);
