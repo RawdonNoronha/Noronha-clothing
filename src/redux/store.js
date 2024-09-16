9b33f75f26e2d70d5db07301e648f228bd76eb5e
RawdonNoronha <rawdonnoronha11@gmail.com>
2024-09-17 00:08:02 +0530

    testcommits

--- src/redux/store.js
+++ src/redux/store.js
@@ -6,9 +6,18 @@ import { persistStore } from "redux-persist";
 const middlewares = [logger];
 
 //process.env is an eviroment variable it will know if the app is working in production or not
-if(process.env.NODE_ENV === 'development')
-{
-    middlewares.push(logger);
+// if(process.env.NODE_ENV === 'development')
+// {
+//     middlewares.push(logger);
+// }
+if (process.env.NODE_ENV === 'development') {
+    // Development-specific code, like logging
+    console.log('Running in development mode');
+}
+
+if (process.env.NODE_ENV === 'production') {
+    // Production-specific code
+    console.log('Running in production mode');
 }
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
