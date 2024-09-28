0c35452ca90becbcd84db672bbc1de24a03991bf
RawdonNoronha <rawdonnoronha11@gmail.com>
2024-09-28 11:03:06 +0530

    fixed netlify

--- src/firebase/firebase.util.js
+++ src/firebase/firebase.util.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { doc, getDoc, getFirestore } from "firebase/firestore";
-import { setDoc } from "firebase/firestore"; 
+import { setDoc } from "firebase/firestore";
 
 // TODO: Replace the following with your app's Firebase project configuration
 // See: https://support.google.com/firebase/answer/7015592
@@ -30,56 +30,55 @@ const provider = new GoogleAuthProvider();
 export const auth = getAuth();
 // console.log('auth', auth);
 
-export const signInWithGoogle = () => signInWithPopup(auth, provider)
+export const signInWithGoogle = () => signInWithPopup(auth, provider);
 
 export const createNewUserInDB = async (user) => {
   try {
-        // Reference the user document in the Firestore database
-        const docRef = doc(db, "users", user.uid);
-        
-        // Fetch the document snapshot
-        const docSnap = await getDoc(docRef);
-        
-        if (docSnap.exists()) {
-          console.log("Document Found!...");
-          
-          // You can set the document data in the state or perform other actions here
-        } else {
-          console.log("No such document!...")
+    // Reference the user document in the Firestore database
+    const docRef = doc(db, "users", user.uid);
 
-          try {
-             await setDoc(docRef, {
-              displayName: user.displayName,
-              email: user.email,
-              createdAt: new Date()
-            });
-            console.log("Document written with ID: ", docRef.id);
-          } catch (e) {
-            console.error("Error adding document: ", e);
-          }
-        }
-      } catch (error) {
-        console.error("Error fetching document:", error);
+    // Fetch the document snapshot
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      console.log("Document Found!...");
+
+      // You can set the document data in the state or perform other actions here
+    } else {
+      console.log("No such document!...");
+
+      try {
+        await setDoc(docRef, {
+          displayName: user.displayName,
+          email: user.email,
+          createdAt: new Date(),
+        });
+        console.log("Document written with ID: ", docRef.id);
+      } catch (e) {
+        console.error("Error adding document: ", e);
+      }
     }
-}
+  } catch (error) {
+    console.error("Error fetching document:", error);
+  }
+};
 
-export const convertCollectionsSnapshotToMap = ( collections ) => {
-  const transformedCollections = collections.docs.map(doc => {
+export const convertCollectionsSnapshotToMap = (collections) => {
+  const transformedCollections = collections.docs.map((doc) => {
     const { title, items } = doc.data();
     return {
       id: doc.id,
       title,
       routeName: encodeURI(title.toLowerCase()),
-      items
-    }
-  })
-  console.log(transformedCollections, 'transformedCollections');
-  return transformedCollections.reduce((accumulator, collection) =>{
+      items,
+    };
+  });
+  console.log(transformedCollections, "transformedCollections");
+  return transformedCollections.reduce((accumulator, collection) => {
     accumulator[collection.title.toLowerCase()] = collection;
     return accumulator;
   }, {});
-  
-}
+};
 
 //for adding shop data in firebase store db
 
